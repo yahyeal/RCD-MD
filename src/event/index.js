88019d0c9a8992fa7922f8b67c0f31b082469448
RCD <Renderuser7739@gmail.com>
2024-11-23 15:26:59 +0530

    𝐑𝐂𝐃 𝐌𝐃 𝐁𝐎𝐓 🚨

--- src/event/index.js
+++ src/event/index.js
@@ -1,5 +1,5 @@
-import Handler from './handler.js'
-import Callupdate from './call-handler.js'
-import GroupUpdate from './group-handler.js'
+import Handler from './handler.js';
+import Callupdate from './call-handler.js';
+import GroupUpdate from './group-handler.js';
 
 export { Handler, Callupdate, GroupUpdate };
