1de57e93583799fe6f7ec8297b0d2e688bccb008
yahyeal <167623344+yahyeal@users.noreply.github.com>
2024-11-29 10:01:20 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "RCD-MD&MPxvv2EF",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
   STATUS_SEND_RAW_URL: process.env.STATUS_SEND_RAW_URL || "",
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
@@ -20,8 +20,8 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   PREFIX: process.env.PREFIX || "!",
-  OWNER_NAME: process.env.OWNER_NAME || "",
-  SUDO_NUMBER: process.env.OWNER_NUMBER || "",
+  OWNER_NAME: process.env.OWNER_NAME || "SHANUWA ID",
+  SUDO_NUMBER: process.env.OWNER_NUMBER || "+94774609569",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
